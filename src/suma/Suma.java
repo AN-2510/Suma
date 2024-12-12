d9404bdc6957c6b2aee31ace1bbb1b30a4f89415
AN-2510 <jagg20023110@gmail.com>
2024-12-11 21:55:08 -0600

    Mi primer commit

---
+++ src/suma/Suma.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package suma;
+
+/**
+ *
+ * @author Dell i5
+ */
+public class Suma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         new HelloWorld().setVisible(true);
+    }
+    
+}
